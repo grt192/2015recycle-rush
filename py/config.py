4db2f37858a75a39034d0917fa70d68a7335d5df
Alex Mallery <amallery@gmail.com>
2015-01-07 22:26:15 -0800

    Initial roboRIO port of 2014 code.

---
+++ py/config.py
@@ -0,0 +1,97 @@
+"""
+Config File for Robot
+"""
+
+__author__ = "Sidd Karamcheti"
+
+from wpilib import Solenoid, Compressor, DriverStation
+import wpilib
+
+from grt.sensors.attack_joystick import Attack3Joystick
+from grt.sensors.xbox_joystick import XboxJoystick
+from grt.sensors.gyro import Gyro
+from grt.core import SensorPoller #, Constants
+from grt.mechanism.drivetrain import DriveTrain
+from grt.mechanism.drivecontroller import ArcadeDriveController
+from grt.mechanism.motorset import Motorset
+from grt.sensors.ticker import Ticker
+from grt.sensors.encoder import Encoder
+from teleop_controller import TeleopController
+from grt.sensors.talon import Talon
+
+#import grt.networktables as networktables
+
+
+#Pin/Port map
+#Talons
+dt_right = Talon(0) #Motorset((Talon(0), Talon(1)), scalefactors=(-1, 1))
+dt_left = Talon(1) #Motorset((Talon(2), Talon(3)), scalefactors=(-1, 1))
+talon_arr = [dt_right, dt_left]
+
+
+#Solenoids + Relays
+compressor_pin = 1
+dt_shifter = Solenoid(1)
+
+#Digital Sensors
+#left_encoder = Encoder(3, 4, constants['dt_dpp'], reverse=True)
+#right_encoder = Encoder(1, 2, constants['dt_dpp'])
+pressure_sensor_pin = 14
+
+#Analog Sensors
+gyro = Gyro(1)
+
+# Controllers
+driver_stick = Attack3Joystick(0)
+xbox_controller = XboxJoystick(1)
+
+#DT
+dt = DriveTrain(dt_left, dt_right, dt_shifter,
+                left_encoder=None, right_encoder=None)
+ac = ArcadeDriveController(dt, driver_stick)
+
+#Sensor Pollers
+sp = SensorPoller((gyro,))
+hid_sp = SensorPoller((driver_stick, xbox_controller))  # human interface devices
+
+ds = DriverStation.getInstance()
+
+teleop_controller = TeleopController(driver_stick, ac, talon_arr, sp, hid_sp)
+
+
+"""
+#Compressor
+compressor = Compressor(pressure_sensor_pin, compressor_pin)
+compressor.Start()
+"""
+
+#Mechs
+
+#Teleop Controllers
+"""
+#Network Tables
+#vision_table = networktables.get_table('vision')
+#status_table = networktables.get_table('status')
+"""
+
+"""
+#Diagnostic ticker
+def status_tick():
+    status_table['l_speed'] = dt.left_motor.Get()
+    status_table['r_speed'] = dt.right_motor.Get()
+    status_table['voltage'] = ds.GetBatteryVoltage()
+    status_table['status'] = 'disabled' if ds.IsDisabled() else 'teleop' if ds.IsOperatorControl() else 'auto'
+
+status_ticker = Ticker(.05)
+status_ticker.tick = status_tick
+
+
+def reset_tick():
+    if driver_stick.button10 and driver_stick.button11:
+        constants.poll()
+
+reset_ticker = Ticker(1)
+reset_ticker.tick = reset_tick
+"""
+#Autonomous
+
---
+++ py/robot.py
@@ -0,0 +1,69 @@
+__author__ = "Sidd Karamcheti, Calvin Huang, Alex Mallery"
+
+import wpilib
+import time
+
+try:
+    auto = basicauto
+except NameError:
+    auto_exists = False
+
+
+class MyRobot(wpilib.SampleRobot):
+    def __init__(self):
+        super().__init__()
+        import config
+        self.sp = config.sp
+        self.hid_sp = config.hid_sp
+        self.dt = config.dt
+        self.ds = config.ds
+        self.teleop_controller = config.teleop_controller
+
+
+    def disabled(self):
+        if auto_exists:
+            auto.stop_autonomous()
+        while self.isDisabled():
+            tinit = time.time()
+            self.sp.poll()
+            time.sleep(0.04 - (time.time() - tinit))
+            #wpilib.Wait(0.04 - (time.time() - tinit))
+    if auto_exists:
+        def autonomous(self):
+            global auto
+            self.dt.up_shift()
+            #self.watchdog.SetEnabled(False)
+
+            if ds.getDigitalIn(1):
+                auto = twoballauto
+            else:
+                auto = basicauto
+
+            auto.run_autonomous()
+            while self.isAutonomous() and self.isEnabled():
+                tinit = time.time()
+                self.sp.poll()
+                time.sleep(0.04 - (time.time() - tinit))
+            auto.stop_autonomous()
+    else:
+        def autonomous(self):
+            pass
+    
+    def operatorControl(self):
+        if auto_exists:
+            auto.stop_autonomous()
+        self.dt.downshift()  # start in low gear for tele
+        #dog = self.getWatchdog()
+        #dog.setExpiration(0.25)
+        #dog.setEnabled(True)
+
+
+        while self.isOperatorControl() and self.isEnabled():
+            #dog.Feed()
+            tinit = time.time()
+            self.teleop_controller.poll()
+            time.sleep(0.04 - (time.time() - tinit))
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
