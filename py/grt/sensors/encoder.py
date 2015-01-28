becab9fe0bd5e2d3159e8ce01f2de18d6c0fea6e
amallery <amallery@gmail.com>
2015-01-27 22:40:51 -0800

    Separated recording macro and drive macro testing into a new branch.

--- py/grt/sensors/encoder.py
+++ py/grt/sensors/encoder.py
@@ -1,3 +1,4 @@
+__author__ = "Calvin Huang"
 
 from wpilib.interfaces import CounterBase
 from wpilib import Encoder as WEncoder
--- py/robot.py
+++ py/robot.py
@@ -1,32 +1,60 @@
+__author__ = "Sidd Karamcheti, Calvin Huang, Alex Mallery"
 
 
 import wpilib
+import inspect
+
 import time
+
+auto = None
+auto_exists = False
 
 
 class MyRobot(wpilib.SampleRobot):
     def __init__(self):
         super().__init__()
         import config
+        
         self.sp = config.sp
         self.hid_sp = config.hid_sp
+        self.dt = config.dt
         self.ds = config.ds
+        self.teleop_controller = config.teleop_controller
+        global auto
+        global auto_exists
+        try:
+            auto = config.basic_auto
+        except AttributeError:
+            auto_exists = False
 
 
     def disabled(self):
+        global auto_exists
+        if auto_exists:
+            auto.stop_autonomous()
         while self.isDisabled():
             tinit = time.time()
             self.sp.poll()
             self.safeSleep(tinit, .04)
-    
-    def autonomous(self):
-        pass
+    if auto_exists:
+        def autonomous(self):
+            global auto
+            auto.run_autonomous()
+            while self.isAutonomous() and self.isEnabled():
+                tinit = time.time()
+                self.sp.poll()
+                self.safeSleep(tinit, .04)
+            auto.stop_autonomous()
+    else:
+        def autonomous(self):
+            pass
     
     def operatorControl(self):
+        if auto_exists:
+            auto.stop_autonomous()
         while self.isOperatorControl() and self.isEnabled():
             tinit = time.time()
-            self.sp.poll()
-            self.hid_sp.poll()
+            self.teleop_controller.poll()
             self.safeSleep(tinit, .04)
             
     def safeSleep(self, tinit, duration):
