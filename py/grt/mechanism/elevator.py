7b6746777934d5ecec887be49934b03b183b86e2
Armin Namavari <ajnamavari12@gmail.com>
2015-02-03 18:05:30 -0800

    Elevator winch servo stuff added

--- py/grt/mechanism/elevator.py
+++ py/grt/mechanism/elevator.py
@@ -1,15 +1,29 @@
 class Elevator:
-    def __init__(self, elevator_motor, elevator_encoder):
+    def __init__(self, elevator_motor, elevator_encoder, winch_servo=None):
         self.elevator_motor = elevator_motor
         self.elevator_encoder = elevator_encoder
+        self.winch_servo = winch_servo
 
     def elevate(self):
+        if self.winch_servo:
+            self.release_winch()
         self.elevator_motor.set(1)
 
     def lower(self):
+        if self.winch_servo:
+            self.release_winch()
         self.elevator_motor.set(-1)
 
     def stop(self):
+        if self.winch_servo:
+            self.engage_winch()
         self.elevator_motor.set(0)
+
     def elevate_speed(self, power):
     	self.elevator_motor.set(power)
+
+    def engage_winch(self):
+        self.winch_servo.setAngle(45)
+
+    def release_winch(self):
+        self.winch_wervo.setAngle(0)
