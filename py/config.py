101817a0c660547dbb1eca45baf2e4ee725fa2a5
amallery <amallery@gmail.com>
2015-02-28 04:03:50 -0800

    Stack macro

--- py/config.py
+++ py/config.py
@@ -118,7 +118,7 @@ record_macro = RecordMacro(talon_arr)
 
 basic_auto = BasicAuto(dt, elevator, talon_arr_basic)
 one_bin_steal = OneBinSteal(talon_arr)
-playback_macro = basic_auto.playback_macro
+playback_macro = one_bin_steal.playback_macro
 
 
 
